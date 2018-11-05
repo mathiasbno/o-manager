c3496b444a14e45e8107c18ad7a2f284a5475a0f
Mathias Benjaminsen <mabenjam@gmail.com>
2018-11-05 09:19:47 +0100

    Add temporary test function for calculating price

--- processing/index.js
+++ processing/index.js
@@ -1,5 +1,5 @@
 const EventorApi = require("./eventor-api/index.js");
-const { saveData, saveConnectedData } = require("./database/index.js");
+const { saveData, getData, saveConnectedData } = require("./database/index.js");
 const { processRelayEvent, processEvent } = require("./helpers/eventHelper.js");
 const { asyncForEach } = require("./helpers/helper.js");
 const { calculatePrice } = require("./helpers/priceHelper.js");
@@ -76,6 +76,38 @@ function processAndSaveEvent(eventor, eventId, dryrun = false) {
     }
   });
 }
+
+// TODO: Move this in its seperate function for price calculation
+// We need to wait for the server to restart due to nodemon,
+// this will not be a problem when deploying
+setTimeout(function() {
+  getData(`${process.env.API_URL}/runner/5bdf653bc3f70081fd5b341a`)
+    .then(data => {
+      const position = data.results[0].legPosition;
+      const event = data.results[0].event;
+      const newPrice = calculatePrice(
+        position,
+        "5bdf7a5b23c05f9a3e895f4a",
+        event._id,
+        event.eventForm
+      );
+
+      data.price.push(newPrice);
+
+      console.log(data);
+
+      saveData(`${process.env.API_URL}/runner`, data)
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}, 400);
 
 // processAndSaveRelayEvent(EventorSE, 17220, true);
 // processAndSaveEvent(EventorNO, 8070, true);
