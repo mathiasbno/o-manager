fd4415af3639fd3ebfd2d7f5550c4937a4547ab7
Mathias Benjaminsen <mabenjam@gmail.com>
2018-11-11 21:21:24 +0100

    Add more delete actions

--- server/routes/nation.mjs
+++ server/routes/nation.mjs
@@ -1,7 +1,7 @@
 import nationController from "../controllers/nation.ctrl.mjs";
 
 export default router => {
-  // router.route("/nations/delete").get(nationController.deleteNation);
+  router.route("/nations/delete").get(nationController.deleteNation);
 
   /* GET */
   router.route("/nations").get(nationController.getAll);
--- server/routes/team.mjs
+++ server/routes/team.mjs
@@ -1,7 +1,7 @@
 import teamController from "../controllers/team.ctrl.mjs";
 
 export default router => {
-  // router.route("/teams/delete").get(teamController.deleteTeams);
+  router.route("/teams/delete").get(teamController.deleteTeams);
 
   /* GET */
   router.route("/teams").get(teamController.getAll);
