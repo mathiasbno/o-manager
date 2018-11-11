82a0887842689588be3b72dd7964d626e88fc8f0
Mathias Benjaminsen <mabenjam@gmail.com>
2018-11-11 21:28:58 +0100

    Reorder port prioritization

--- server/server.mjs
+++ server/server.mjs
@@ -6,7 +6,7 @@ import routes from "./routes/index.mjs";
 
 const app = express();
 const router = express.Router();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const url = process.env.MONGODB_URI || "mongodb://localhost:27017/o-manager";
 
 try {
